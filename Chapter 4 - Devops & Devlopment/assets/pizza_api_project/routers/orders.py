3a3e090e617f226ccce8207b49b90bdc7cb50ddc
Agama16 <126578288+Agama16@users.noreply.github.com>
2026-04-15 17:08:43 +0300

    removed to do comment

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/routers/orders.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/routers/orders.py
@@ -14,13 +14,6 @@ def get_menu():
 
 @router.post("/orders")
 def create_order(order: OrderRequest):
-    """
-    TODO: INCOMPLETE ENDPOINT!
-    1. Calculate total price.
-    2. Call 'save_order_to_db(order_data)' to save it.
-    3. Return a success message with the total price and an order ID.
-    4. Handle cases where the pizza list is empty (raise 400 exception).
-    """
 
     # check if list is empty, is so raise an exception
     if len(OrderRequest.pizzas) == 0 :

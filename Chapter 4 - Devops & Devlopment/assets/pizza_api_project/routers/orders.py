81f6aadc9206a6c54a3e4727df0dcfb6c081966a
Agama16 <126578288+Agama16@users.noreply.github.com>
2026-04-17 00:08:12 +0300

    Update orders.py

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/routers/orders.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/routers/orders.py
@@ -15,32 +15,31 @@ def get_menu():
 @router.post("/orders")
 def create_order(order: OrderRequest):
 
-    # check if list is empty, is so raise an exception
+
     if len(OrderRequest.pizzas) == 0 :
         raise HTTPException(status=400, detail="list is empty")
 
     else:
 
-        # calc the total price of the pizzas
+
         for pizza in OrderRequest.pizzas:
             total_price+=pizza.price
-
-        # create a dictionary with the order details 
-        cust_id=str(uuid.uuid4())  #generate a randome id 
+ 
+        cust_id=str(uuid.uuid4())  
         order={
             "customer_id" : cust_id, 
             "customer" : OrderRequest.customer_name,
             "pizzas_list" : OrderRequest.pizzas,
         }
 
-        # save order to db
+    
         saved = save_order_to_db(order)
 
-        # check if the order was actually saved, if not raise an exception
+        
         if not saved :
             raise HTTPException(status=400, detail="order failed")
 
-        # if the order was saved, send confirmation
+        
         return[
 
             {"Order saved!, total price:" : total_price},

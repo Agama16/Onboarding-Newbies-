2ed0a53611a978d5b57660ad34623c7f4d8b3ee3
Agama16 <126578288+Agama16@users.noreply.github.com>
2026-04-17 23:52:13 +0300

    Update test_main.py

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/tests/test_main.py
@@ -1,52 +1,54 @@
 from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, patch
+from typing import Any, Dict
 from main import app
-from models.pizza import OrderRequest
-from unittest.mock import patch
 
-client = TestClient(app)
+client: TestClient = TestClient(app)
 
-def test_get_menu():
+
+def test_get_menu() -> None:
     response = client.get("/menu")
+
     assert response.status_code == 200
     assert len(response.json()) == 3
     assert response.json()[0]["name"] == "Margherita"
 
-@patch('main.save_order_to_db')
-def test_create_order_success(mock_save_db):
 
+@patch("main.save_order_to_db")
+def test_create_order_success(mock_save_db: MagicMock) -> None:
     
-    mock_order={
-        "customer_name" : "bob",
-        "pizzas" : [ "Margherita", 10.0]
+    mock_order: Dict[str, Any] = {
+        "customer_name": "bob",
+        "pizzas": ["Margherita", 10.0],
     }
-
     mock_save_db.return_value = True
 
-   
-    response = client.post('/order', mock_order)
+    response = client.post("/order", json=mock_order)
     assert response.status_code == 200
+
+
+@patch("main.save_order_to_db")
+def test_create_order_fail_nonexistent_item(mock_save_db: MagicMock) -> None:
     
+    mock_order: Dict[str, Any] = {
+        "customer_name": "john",
+        "pizzas": ["none", 1.0],
+    }
+    mock_save_db.return_value = False
+    
+    response = client.post("/order", json=mock_order)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "order failed"
 
-@patch('main.save_order_to_db')
-def test_create_order_fail(mock_save_db):  
 
-    mock_order={
-        "customer_name" : "john", "pizzas" : [ "none", 1.0]
+def test_create_order_fail_empty_list() -> None:
+    
+    order: Dict[str, Any] = {
+        "customer_name": "testOrder",
+        "pizzas": [],
     }
 
- 
-    mock_save_db.return_value = False
-
-    response = client.post('/order', mock_order)
-    assert response.status_code == 400
-    assert response.detail == "order failed"
-
-def test_create_order_empty_list():
-
-    
-    order = OrderRequest("testOrder", [])
     response = client.post("/order", json=order)
     assert response.status_code == 400
-    assert response.detail == "list is empty"
-
-
+    assert response.json()["detail"] == "list is empty"
+    
